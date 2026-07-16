a86a85f41a56550087d7b7aa55e7135ddecda537
Diksha Ravindra Nirbhavane <159103865+nirbhavanediksha8@users.noreply.github.com>
2026-07-16 10:56:59 +0530

    Update Login.jsx

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -5,6 +5,25 @@ import {Link} from 'react-router-dom'
 import Register from './pages/Register'
 
 const Login = () => {
+    const[email, setEmail] = useState("");
+    const[password, setPassword] = useState("");
+    const[msg, setMsg] = useState("");
+
+    const handleLogin = async (e) =>{
+        e.preventDefault();
+        try{
+            const res = await axios.post("http://localhost:5000/api/login",{email, password})
+            setMsg(res.data.msg);
+        }
+        catch(err){
+            setMsg(err.response.data.msg);
+        }
+    }
+
+    const handleChange = (e) =>{
+        const{name, value} = e.target;
+    }
+
   return (
     <div className="login">
         <h1>Login</h1>
@@ -14,7 +33,7 @@ const Login = () => {
             <input type = "password" placeholder = "Enter Password" value = {password} onChange = {(e) => setPassword(e.target.value)}/>
 
             <button type="submit">Login</button>
-            <p>Don't have an account? <Link to = "/Register">Register Here</Link></p>
+            <p>Don't have an account? <Link to = "/register">Register Here</Link></p>
         </form>
     </div>
   )
