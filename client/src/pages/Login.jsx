a13b394c89d4b8424bb2116288c08d194268d375
Diksha Ravindra Nirbhavane <159103865+nirbhavanediksha8@users.noreply.github.com>
2026-07-16 10:34:37 +0530

    Update Login.jsx

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import {useState} from 'react'
 import axios from 'axios'
 import {Link} from 'react-router-dom'
+import Register from './pages/Register'
 
 const Login = () => {
   return (
